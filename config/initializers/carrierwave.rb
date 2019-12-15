4f988340574f0c4a6c8fc4c8c9e26156af78543b
IiyamaMakoto <m.iiyama@gmail.com>
2019-12-15 12:07:29 +0900

    Create carrierwave.rb
    s3にアップロードできるよう認証情報を入力しました。
    テスト環境、開発環境ではローカルに保存され、本番環境ではs3に保存するように分岐しています。

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+if Rails.env.test? || Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
+elsif Rails.env.production?
+  CarrierWave.comfigure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region:                'ap-northeast-1'
+    }
+    config.fog_directory  = 'freemarket-sample-59wa'
+    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/freemarket-sample-59wa'
+  end
+end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:print:]]/ #ファイル名の文字化け防止
