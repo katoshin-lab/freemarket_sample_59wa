15636f44d7c026937b27e2ec6c452313f6bed3c3
IiyamaMakoto <m.iiyama@gmail.com>
2019-12-18 15:46:15 +0900

    Update routes.rb
    resourcesを使っての記述に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items, only: [:index, :new]
 
   root to: 'items#index'
 end
