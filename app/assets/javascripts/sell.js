7b75db60795a81a9bfccb7a1dd85f67f12e1f819
IiyamaMakoto <m.iiyama@gmail.com>
2019-12-19 21:24:45 +0900

    choose is_shipping -> show method

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,4 +1,39 @@
 $(function() {
+
+
+  $('#is_seller_shipping').on('change', function() {
+    var seller_shipping = $('#is_seller_shipping option:selected').val()
+    if ( seller_shipping == "" ) {
+      $('#method').hide();
+    } else if (seller_shipping == "true") {
+      var HTML = `
+        <option value>---</option>
+        <option value="1">未定</option>
+        <option value="2">らくらくメルカリ便</option>
+        <option value="3">ゆうメール</option>
+        <option value="4">レターパック</option>
+        <option value="5">普通郵便(定形、定形外)</option>
+        <option value="6">クロネコヤマト</option>
+        <option value="7">ゆうパック</option>
+        <option value="8">クリックポスト</option>
+        <option value="9">ゆうパケット</option>
+      `
+      $('#shipping_method').empty().append(HTML);
+      $('#method').show();
+    } else {
+      var HTML = `
+        <option value>---</option>
+        <option value="1">未定</option>
+        <option value="6">クロネコヤマト</option>
+        <option value="7">ゆうパック</option>
+        <option value="3">ゆうメール</option>
+      `
+      $('#shipping_method').empty().append(HTML);
+      $('#method').show();
+
+    }
+  })
+
   $('#price').on("keyup", function(){
     var input = $('#price').val();
     var charge = Math.floor(input * 0.1);
