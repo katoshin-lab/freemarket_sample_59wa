e57c633e23ea52ca3810a63eb3f56844aa428cf6
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-12-19 16:53:23 +0900

    Add asoociation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :items, foreign_key: "seller_id"
-  has_many :items, foreign_key: "buyer_id"
+  has_one :user_address
+  has_one :user_real_name
+  has_one :user_delivery
+  has_many :user_payments
+  has_many :likes
+  has_many :reports
+  has_many :items
+  has_many :dealing_items
+  has_many :bought_items
 end
