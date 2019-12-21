65fa408d85edcc20eb326abc97095479674484cf
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-12-21 16:06:54 +0900

    edit sms-identification
    SMS認証コード確認用のコントローラーと仮のビュー

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -23,6 +23,20 @@ class SignupsController < ApplicationController
     redirect_to sms_confirmation_signups_path
   end
 
+  def sms_confirmation
+  end
+
+  def sms_check
+    @user_identification = UserIdentification.new
+    confirmation_number = params[:confirmation_number]
+    binding.pry
+    if confirmation_number.to_i == session[:sms_number]
+      redirect_to new_address_path
+    else
+      render "signups/sms_confirmation"
+    end
+  end
+
   protected
   def user_identifcation_params
     params.permit(:mobile_phone_number)
