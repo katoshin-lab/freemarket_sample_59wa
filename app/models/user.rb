03ac740da4ad1d9e8d75b78ab37449ad8976828c
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-12-21 16:04:17 +0900

    edit association
    テーブル名変更に伴うアソシエーション名の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one :user_address
-  has_one :user_real_name
+  has_one :user_identification
   has_one :user_delivery
   has_many :user_payments
   has_many :likes
