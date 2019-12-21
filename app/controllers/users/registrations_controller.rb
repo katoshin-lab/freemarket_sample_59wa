c467f4caa2a4cbe94ffc277a194b025e89ea000d
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-12-21 16:34:35 +0900

    Update user-registration controller
    ストロングパラメーターに追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -44,6 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :birthday])
+    params.permit(:last_name, :first_name, :last_name_kana, :first_name_kana)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
