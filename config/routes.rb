70da18ee5a7595f6ebeab017a12d2d353416a9b4
tomtech59 <shimanokan@gmail.com>
2019-12-21 19:48:17 +0900

    replace erb to haml
    erbファイルをhamlに変換し、新規登録画面とログイン画面をdeviseのviewに一部差し替えました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
   resources :items, only: [:index, :new]
-
-  resources :signups, only: [:new]
-  get "signups/login" => "signups#login"
 end
