a7d5f2cff7c1a25fe2669bf71ac390eaaec4f12f
IiyamaMakoto <m.iiyama@gmail.com>
2019-12-24 12:56:09 +0900

    multiple image upload view

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,20 +1,70 @@
 $(function() {
 
-  $('#upload_image').on('change', $(this), function(e) {
-    file = e.target.files[0]
-    reader = new FileReader();
+  $('#dropbox').on('change', $('.file_field'), function(e) {
+    var number = Number($('#number').text());
+    var total = Number($('#total').text());
+    if (total < 10) {
+      number += 1;
+      total += 1;
+      var image_number = '#upload_image'+number
+      var buildHTML = `
+        <div class="sell__image_box">
+          <div class="sell__image" id="upload_image${number}">
+          </div>
+          <div class="sell__image_btn--delete" id=${number}>
+            削除
+          </div>
+        </div>
+      `
+      var buildForm = `<input id="image${number+1}" multiple="multiple" class="file_field" type="file" name="image[]">`;
 
-    reader.onload = (function(file) {
-      return function(e) {
-        $("#image1").append($('<img>').attr({
-          src: e.target.result,
-          title: file.name
-        }));
-      };
-    })(file);
-    reader.readAsDataURL(file);
+      file = e.target.files[0];
+      reader = new FileReader();
+      $('#container').append(buildHTML).trigger('create');
+      if (total === 5){
+        $('#dropbox').css('width', 620);
+      } else if (total === 10) {
+        $('#dropbox').hide();
+      } else {
+        var box_width = $('#dropbox').css('width').replace('px', '');
+        box_width -= 126;
+        $('#dropbox').css('width', box_width+'px');
+      }; 
+
+      reader.onload = (function(file) {
+        return function(e) {
+
+          $(image_number).prepend($('<img>').attr({
+            src: e.target.result,
+            title: "image" + number,
+            class: "sell__image"
+          }));
+        };
+      })(file);
+      reader.readAsDataURL(file);
+      $('#dropbox').append(buildForm).trigger('create');
+      $('#number').text(number);
+      $('#total').text(total);
+    };
   });
-
+  $('#container').click('.sell__image_btn--delete', function() {
+    var total = Number($('#total').text());
+    var click = $(event.target);
+    var click_image_id = "#image" + click.attr('id');
+    if (total === 10) {
+      $('#dropbox').show();
+    } else if (total === 5) {
+      $('#dropbox').css('width', 116);
+    } else {
+      var box_width = $('#dropbox').css('width').replace('px', '');
+      box_width = Number(box_width) + 126;
+      $('#dropbox').css('width', box_width);
+    }
+    click.parent().remove();
+    $(click_image_id).remove();
+    total--;
+    $('#total').text(total).trigger('create');
+  });
 
   $('#is_seller_shipping').on('change', function() {
     var seller_shipping = $('#is_seller_shipping option:selected').val()
