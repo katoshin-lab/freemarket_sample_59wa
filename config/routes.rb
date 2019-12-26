1d9c1160ca9796496fc1685bbb2df1b681fcfeac
takemoto715 <falloutboy174@gmail.com>
2019-12-26 19:59:42 +0900

    add routing conroller
    仮のルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, module: :users
   root 'homes#index'
+  get  "mypages/logout"  => "mypages#logout"
   resources :items, only: [:index, :new]
   resources :mypages, only: [:index]
 end
