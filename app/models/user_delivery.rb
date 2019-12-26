4384a3aeb583c95e93117b27fa12db837f452e23
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-12-26 19:40:46 +0900

    Add validation to user_delivery model

--- app/models/user_delivery.rb
+++ app/models/user_delivery.rb
@@ -2,4 +2,6 @@ class UserDelivery < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  validates :postal_number, format: {with: /\A[0-9]{7}\z/}
+  validates :phone_number, format: {with: /\A[0-9]{10,11}\z/}
 end
