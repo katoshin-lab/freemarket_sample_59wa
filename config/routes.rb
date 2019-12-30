d575713e59b14400dc2abc269fde55d7ef5adf0f
tomtech59 <shimanokan@gmail.com>
2019-12-30 20:46:31 +0900

    add to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users, module: :users
   root 'homes#index'
   resources :items, only: [:index, :new]
   resources :mypages, only: [:index, :new]
-  resources :signups, only: [:new]
+  resources :signups, only: [:new]　do
 
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    confirmations: "users/confirmations",
+    sessions:      "users/sessions"
+  }
+  resources :signups do
+    collection do
+      get :sms_authentication
+      post :sms_post
+      get :sms_confirmation
+      post :sms_check
+      get :complete
+    end
+  end
+  resources :deliveries, only: [:new, :create, :update]
+  resources :payments, only: [:new, :create, :update, :destroy]
+  root 'tests#index'
 end
