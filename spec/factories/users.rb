09d2883723086da04aec489b12897160bf781658
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-03 17:26:49 +0900

    Add factoryBot user instance

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    name                     {"testname"}
+    email                    {"test@gmail.com"}
+    password                 {"0123456"}
+    last_name                {"山田"}
+    first_name               {"太郎"}
+    last_name_kana           {"ヤマダ"}
+    first_name_kana          {"タロウ"}
+    mobile_phone_number      {"09012345678"}
+    birthday                 {"19900101"}
+    profile                  {"こんにちは"}
   end
 end
