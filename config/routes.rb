d7d1885295b35373852664e8a8c6f670fe1717fd
takemoto715 <falloutboy174@gmail.com>
2020-01-03 10:59:13 +0900

    fix routing
    ルーティング部分の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, module: :users
   root 'homes#index'
   resources :items, only: [:index, :new]
-  resources :mypages do
+  resources :mypages, only: [:index] do
     collection do
       get :logout
       get :profile
     end
   end
-  resources :mypages, only: [:index, :new, :show]
   resources :signups, only: [:new]
 
 end
