397268b893aa147d9a78697fea21b1b1086d33d5
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-04 21:02:22 +0900

    Add user test can save

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @test_user = build(:user)
+    end
+    context "can save" do
+      it "is valid with a name, email, password, last_name, first_name, last_name_kana, first_name_kana, mobile_phone_number, birthday and profile" do
+        expect(@test_user).to be_valid
+      end
+
+      it "is valid without mobile_phone_number and profile" do
+        @test_user.mobile_phone_number = nil
+        @test_user.profile = nil
+        expect(@test_user).to be_valid
+      end
+    end
+
 end
