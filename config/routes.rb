38f8c03e282803e635faf12f2785d64e96cb12d1
IiyamaMakoto <m.iiyama@gmail.com>
2020-01-05 12:21:45 +0900

    make buying check_page_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, module: :users
   root 'homes#index'
   resources :items, only: [:index, :new]
+  resources :buy, only: [:show]
   resources :signups, only: [:new]
   resources :mypages, only: [:index]
 end
