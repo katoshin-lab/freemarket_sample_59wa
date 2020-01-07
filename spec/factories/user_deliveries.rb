d3ede5bb6dd02cad7a74619a56310e78c3e6e031
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-07 19:53:52 +0900

    Add factorybot-user_delivery

--- spec/factories/user_deliveries.rb
+++ spec/factories/user_deliveries.rb
@@ -1,5 +1,16 @@
+require 'faker'
+require 'forgery_ja'
+
 FactoryBot.define do
   factory :user_delivery do
-    
+    last_name                { ForgeryJa(:name).last_name }
+    first_name               { ForgeryJa(:name).first_name }
+    last_name_kana           { ForgeryJa(:name).last_name(to: ForgeryJa::KANA) }
+    first_name_kana          { ForgeryJa(:name).first_name(to: ForgeryJa::KANA) }
+    postal_number            { Faker::Number.number(digits: 7) }
+    city                     { ForgeryJa(:address).city }
+    block                    { ForgeryJa(:address).street_number }
+    building_name            { "テストレジデンス" }
+    phone_number             { Faker::Number.number(digits: Faker::Number.between(from: 9, to: 10)) }
   end
 end
