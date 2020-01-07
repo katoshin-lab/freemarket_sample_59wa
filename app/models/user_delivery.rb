6345377f3da81718912ea7666381a3b5ea08d79e
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-07 21:09:32 +0900

    Add validation to user_delivery

--- app/models/user_delivery.rb
+++ app/models/user_delivery.rb
@@ -8,8 +8,9 @@ class UserDelivery < ApplicationRecord
   validates :last_name_kana, presence: true, length: { maximum: 20 }
   validates :first_name_kana, presence: true, length: { maximum: 20 }
   validates :postal_number, presence: true, format: { with: /\A[0-9]{7}\z/ }
+  validates :prefecture_id, presence: true
   validates :city, presence: true, length: { maximum: 25 }
   validates :block, presence: true, length: { maximum: 25 }
-  validates :building_name, length: { maximum: 25 }
-  validates :phone_number, format: { with: /\A[0-9]{9,10}\z/ }
+  validates :building_name, length: { maximum: 25 }, allow_blank: true
+  validates :phone_number, format: { with: /\A[0-9]{9,10}\z/ }, allow_blank: true
 end
