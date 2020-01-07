70143f122792b5a5d998ebb3dea2b21615fff279
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-07 21:10:38 +0900

    Add "can save" tests to user_delivery

--- spec/models/user_delivery_spec.rb
+++ spec/models/user_delivery_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe UserDelivery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @test_user = create(:user)
+      @test_delivery = build(:user_delivery, user_id: @test_user.id)
+    end
+    context "can save" do
+      it "is valid with last_name, first_name, last_name_kana, first_name_kana, postal_number, prefecture_id, city, block, building_name, phone_number and user_id" do
+        expect(@test_delivery).to be_valid
+      end
+
+      it "is valid without a building_name" do
+        @test_delivery.building_name = nil
+        expect(@test_delivery).to be_valid
+      end
+
+      it "is valid without a phone_number" do
+        @test_delivery.phone_number = nil
+        expect(@test_delivery).to be_valid
+      end
+
+      it "is valid without building_name and phone_number" do
+        @test_delivery.building_name = nil
+        @test_delivery.phone_number = nil
+        binding.pry
+        expect(@test_delivery).to be_valid
+      end
+    end
+  end
 end
