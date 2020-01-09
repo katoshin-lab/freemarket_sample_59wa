28e45506847d9227768ddcd2f85001e3e8edebab
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-09 18:47:10 +0900

    Add user_payment tests

--- spec/factories/user_payments.rb
+++ spec/factories/user_payments.rb
@@ -1,5 +1,10 @@
+require 'faker'
+require 'forgery_ja'
+
 FactoryBot.define do
   factory :user_payment do
-    
+    id              { Faker::Number.number(digits: Faker::Number.between(from: 1, to: 10)) }
+    customer_id     { "cus_" + Faker::Crypto.md5 }
+    card_id         { "car_" + Faker::Crypto.md5 }
   end
 end
--- spec/models/user_payment_spec.rb
+++ spec/models/user_payment_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe UserPayment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create' do
+    subject { test_payment.valid? }
+    let(:user) { create(:user) }
+    let(:test_payment) { build(:user_payment, user_id: user.id) }
+    context "can save" do
+      it "is valid with customer_id, card_id and user_id" do
+        expect(test_payment).to be_valid
+      end
+    end
+
+    context "cannot save" do
+      it "is invalid without user_id" do
+        test_payment.user_id = nil
+        subject
+        expect(test_payment.errors[:user_id]).to include("can't be blank")
+      end
+
+      it "is invalid without customer_id" do
+        test_payment.customer_id = nil
+        subject
+        expect(test_payment.errors[:customer_id]).to include("can't be blank")
+      end
+
+      it "is invalid without card_id" do
+        test_payment.card_id = nil
+        subject
+        expect(test_payment.errors[:card_id]).to include("can't be blank")
+      end
+
+      it "is invalid with a customer_id which doesn't begin with 'cus_'" do
+        test_payment.customer_id = "a3a2dd1bf4f9c13c73ff39330eb0"
+        subject
+        expect(test_payment.errors[:customer_id]).not_to match(/\Acus_[0-9a-f]+\z/)
+      end
+
+      it "is invalid with a card_id which doesn't begin with 'car_'" do
+        test_payment.card_id = "51b8c2dbbb23cf4f2c5c8d09588d"
+        subject
+        expect(test_payment.errors[:card_id]).not_to match(/\Acus_[0-9a-f]+\z/)
+      end
+    end
+  end
 end
