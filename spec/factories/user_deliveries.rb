bcb7ce6135493a5187b7669f6036959f83f592ad
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-09 18:45:09 +0900

    Modificate space

--- spec/factories/user_deliveries.rb
+++ spec/factories/user_deliveries.rb
@@ -3,7 +3,7 @@ require 'forgery_ja'
 
 FactoryBot.define do
   factory :user_delivery do
-    id                       { Faker::Number.number(digits: Faker::Number.between(from: 1, to: 10))}
+    id                       { Faker::Number.number(digits: Faker::Number.between(from: 1, to: 10)) }
     last_name                { ForgeryJa(:name).last_name }
     first_name               { ForgeryJa(:name).first_name }
     last_name_kana           { ForgeryJa(:name).last_name(to: ForgeryJa::KANA) }
