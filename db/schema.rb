11de602d51461e388859302efec0b77d3779e270
IiyamaMakoto <m.iiyama@gmail.com>
2020-01-12 23:32:52 +0900

    fix migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_110500) do
+ActiveRecord::Schema.define(version: 2020_01_12_080956) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 40
