43dc8c7d5455ec445b676cc8a2b96e6959dafb08
takemoto715 <falloutboy174@gmail.com>
2020-01-16 14:58:39 +0900

    add validates
    validationの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,8 +17,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_status
 
   
-  validates :name, presence: true
-  validates :detail, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :detail, presence: true, length: { maximum: 1000 }
   validates :seller, presence: true
   validates :category_id, presence: true
   validates :condition_id, presence: true
@@ -26,7 +26,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :shipping_period_id, presence: true
   validates :shipping_method_id, presence: true
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, inclusion: {in: 300..9999999 }
   validates :item_status_id, presence: true
 
 end
