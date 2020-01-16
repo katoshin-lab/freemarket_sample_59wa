448d37569198b6748f6a09c69e6fa7c0f539f4ff
takemoto715 <falloutboy174@gmail.com>
2020-01-16 17:22:35 +0900

    image model test
    imageモデルの単体テスト

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,6 @@ class Image < ApplicationRecord
   belongs_to :item, optional: true
 
   mount_uploaders :image, ImageUploader
+
+  validates :image, presence: true
 end
---
+++ spec/factories/images.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do  
+
+  factory :image do
+    image                  {"sample.jpg"}
+  end
+
+end
