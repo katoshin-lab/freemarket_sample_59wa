2d1310900e8457a18410ce242fe49db9872fd3aa
IiyamaMakoto <m.iiyama@gmail.com>
2020-01-24 07:42:53 +0900

    fix likes function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
 
   def create
-    current_user.likes.create(item_id: params[:item_id])
-  end
-      
-  def destroy
-    current_user.likes.find_by(item_id: params[:item_id]).destroy
+    if @item = current_user.likes.find_by(item_id: params[:item_id])
+      @item.destroy
+    else
+      current_user.likes.create(item_id: params[:item_id])
+    end
   end
 
 end
