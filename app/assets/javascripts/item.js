0c2f952a649f56ee1421ee294aaff989dafb6ced
IiyamaMakoto <m.iiyama@gmail.com>
2020-01-26 23:54:08 +0900

    Update item.js
    いいね関連の表示は非同期通信で更新されるようになったため、不要になったjsによる更新部分を削除しました。

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -6,25 +6,4 @@ $(function() {
     $('.items-image__box').remove();
     $('.items-image').prepend(imageHTML);
   })
-
-  // いいねボタン機能
-  // いいねを追加
-  $('.items-tag__left').on('click', '#good_btn', function() {
-    setTimeout(function(){
-      var likes_count = $('#likes_count').text();
-      $('#good_btn').attr("id", "good_btn_liked");
-      $('#likes_count').text(Number(likes_count) + 1);  
-    }, 50);
-  })
-  // いいね済みの場合はいいねを取り消し
-  $('.items-tag__left').on('click', '#good_btn_liked', function() {
-    setTimeout(function(){
-      var likes_count = $('#likes_count').text();
-      $('#good_btn_liked').attr("id", "good_btn");
-      $('#likes_count').text(Number(likes_count) - 1);
-      if ($('#likes_count').text() === "0") {
-        $('#likes_count').text("");
-      };
-    }, 50);
-  })
 })
