176a80c332ebd4dd2c2108e66f1821ddeb9decd0
takemoto715 <falloutboy174@gmail.com>
2020-01-28 21:00:15 +0900

    fix alert msessages
    アラート分の修正

--- app/controllers/concerns/item_helper.rb
+++ app/controllers/concerns/item_helper.rb
@@ -4,16 +4,10 @@ module ItemHelper
   end
 
   def alert_image
-    { js: "$('#scroll_to_top').click(function() {
-      $('body, html').scrollTop(0);
-    });
-     window.alert('画像が入っていません')" }
+    { js: "window.alert('画像が入っていません');" }
   end
 
   def alert_text
-    { js: "$('#scroll_to_top').click(function() {
-      $('body, html').scrollTop(0);
-    });
-     window.alert('画像が入っていません')" }
+    { js: "window.alert('入力に誤りがあります')" }
   end
 end
