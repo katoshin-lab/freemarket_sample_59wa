60a0db988c35c78514126924d37b662245da4571
IiyamaMakoto <m.iiyama@gmail.com>
2020-01-29 12:31:22 +0900

    Setting item and card info
    購入する商品の情報やcurrent_userのカード情報が表示されるようにしました。

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -1,8 +1,11 @@
 class BuyController < ApplicationController
+  include PaymentsHelper
 
   def show
     @item = Item.find(params[:id])
     @delivery = current_user.user_delivery
+    @payments = current_user.user_payments
+    get_card_info
   end
 
   def update
--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,2 +1,29 @@
 module PaymentsHelper
+  def get_card_info
+    payment = UserPayment.where(user_id: current_user.id).first
+    if payment.present?
+      payjp_setting
+      customer = Payjp::Customer.retrieve(payment.customer_id)
+      @card_info = customer.cards.retrieve(payment.card_id)
+      case @card_info.brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+      end
+    end
+  end
+
+  def payjp_setting
+    Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+  end
+
 end
