23a26a5466d1e87f76f1df2b4e4dcda3abc9b268
IiyamaMakoto <m.iiyama@gmail.com>
2020-01-29 18:55:02 +0900

    Update last_url.js
    ページ遷移をしてからブラウザの「戻る」ボタンを押した際に正しく遷移しない問題を修正しました。

--- app/assets/javascripts/last_url.js
+++ app/assets/javascripts/last_url.js
@@ -2,7 +2,7 @@ $(function() {
   var lastURL = $('#lastURL').val();
   $('#card_change').on('click', function() {
     $('#card_update').on("ajax:success", function(event, xhr, settings) {　
-      window.location.replace(lastURL);
+      window.location.href = lastURL;
     });
     $('#card_update').on("ajax:error", function(event, xhr, settings) {　
       window.alert("エラーが発生しました");
@@ -11,7 +11,7 @@ $(function() {
 
   $('#delivery_change').on('click', function() {
     $('#delivery_update').on("ajax:success", function(event, xhr, settings) {　
-      window.location.replace(lastURL);
+      window.location.href = lastURL;
     });
     $('#delivery_update').on("ajax:error", function(event, xhr, settings) {　
       window.alert("エラーが発生しました");
