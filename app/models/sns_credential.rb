ce474f605d9927f5bdfb0a766624b5d06de22aef
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-01-30 17:49:41 +0900

    Add validation in sns_credential

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,10 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user
+
+  validate :uid, presence: true, unipueness: true, numericality: { only_integer: true }
+  validate :provider, presence: true
+  validate :user_id, numericality: { only_integer: true }, allow_blank: true
+  validate :name, presence: true
+  validate :email,presence: true
+  validate :token, presence: true
 end
