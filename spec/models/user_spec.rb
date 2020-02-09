c9610d6ae28497b1467d75754189f906933ee34a
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2020-02-09 17:25:00 +0900

    cleaning code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,99 +18,99 @@ RSpec.describe User, type: :model do
     context "cannot save" do
       it "is invalid without name" do
         test_user.name = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:name]).to include("can't be blank")
       end
 
       it "is invalid without email" do
         test_user.email = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:email]).to include("can't be blank")
       end
 
       it "is invalid without password" do
         test_user.password = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:password]).to include("can't be blank")
       end
 
       it "is invalid without last_name" do
         test_user.last_name = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:last_name]).to include("can't be blank")
       end
 
       it "is invalid without first_name" do
         test_user.first_name = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:first_name]).to include("can't be blank")
       end
 
       it "is invalid without last_name_kana" do
         test_user.last_name_kana = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:last_name_kana]).to include("can't be blank")
       end
 
       it "is invalid without first_name_kana" do
         test_user.first_name_kana = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:first_name_kana]).to include("can't be blank")
       end
 
       it "is invalid without birthday" do
         test_user.birthday = nil
-        test_user.valid?
+        subject
         expect(test_user.errors[:birthday]).to include("can't be blank")
       end
 
       it "is invalid with a duplicated name" do
         user = create(:user)
         test_user.name = user.name
-        test_user.valid?
+        subject
         expect(test_user.errors[:name]).to include("has already been taken")
       end
 
       it "is invalid with a duplicated email" do
         user = create(:user)
         test_user.email = user.email
-        test_user.valid?
+        subject
         expect(test_user.errors[:email]).to include("has already been taken")
       end
 
       it "is invalid with a name which is more than 21 characters" do
         test_user.name = "aaaaaaaaaabbbbbbbbbbc"
-        test_user.valid?
+        subject
         expect(test_user.errors[:name]).to include("is too long (maximum is 20 characters)")
       end
 
       it "is invalid with a password which is less than 5 characters" do
         test_user.password = 12345
-        test_user.valid?
+        subject
         expect(test_user.errors[:password]).to include("is too short (minimum is 6 characters)")
       end
 
       it "is invalid with a last_name which is more than 21 characters" do
         test_user.last_name = "あいうえおかきくけこさしすせそたちつてとな"
-        test_user.valid?
+        subject
         expect(test_user.errors[:last_name]).to include("is too long (maximum is 20 characters)")
       end
 
       it "is invalid with a first_name which is more than 21 characters" do
         test_user.first_name = "あいうえおかきくけこさしすせそたちつてとな"
-        test_user.valid?
+        subject
         expect(test_user.errors[:first_name]).to include("is too long (maximum is 20 characters)")
       end
 
       it "is invalid with a last_name_kana which is more than 21 characters" do
         test_user.last_name_kana = "アイウエオカキクケコサシスセソタチツテトナ"
-        test_user.valid?
+        subject
         expect(test_user.errors[:last_name_kana]).to include("is too long (maximum is 20 characters)")
       end
 
       it "is invalid with a first_name_kana which is more than 21 characters" do
         test_user.first_name_kana = "アイウエオカキクケコサシスセソタチツテトナ"
-        test_user.valid?
+        subject
         expect(test_user.errors[:first_name_kana]).to include("is too long (maximum is 20 characters)")
       end
 
@@ -128,19 +128,19 @@ RSpec.describe User, type: :model do
 
       it "is invalid with a mobile phone number which is less than 9 digits" do
         test_user.mobile_phone_number = 123456789
-        test_user.valid?
+        subject
         expect(test_user.errors[:mobile_phone_number]).to include("is invalid")
       end
 
       it "is invalid with a mobile phone number which is more than 11 digits" do
         test_user.mobile_phone_number = 12345678901
-        test_user.valid?
+        subject
         expect(test_user.errors[:mobile_phone_number]).to include("is invalid")
       end
 
       it "is invalid with a mobile phone number which includes hyphen" do
         test_user.mobile_phone_number = "090-1234-5678"
-        test_user.valid?
+        subject
         expect(test_user.errors[:mobile_phone_number]).not_to match(/\A[0-9]{10}\z/)
       end
     end
