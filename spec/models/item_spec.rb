84f506eb286158460b3d444b6ad98ca72c0e48f7
takemoto715 <falloutboy174@gmail.com>
2020-02-13 19:40:08 +0900

    fix-indent
    インデントの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,110 +5,110 @@ describe Item do
   describe '#create' do
   subject {test_item.valid?}
   let(:test_item){build(:item)}
-  
-   context 'can save' do
-    it "is vaild with a name , a seller_id, detail , a category_id, condition_id, is_seller_shipping, prefecture_id, shipping_period_id, shipping_method_id , price , item_status_id " do
-    expect(test_item).to be_valid
-    end
-   end
-   
-   context 'cannot save' do
-    it "is invalid without a name" do
-      test_item.name = nil
-      subject
-      expect(test_item.errors[:name]).to include("can't be blank")
+
+    context 'can save' do
+      it "is vaild with a name , a seller_id, detail , a category_id, condition_id, is_seller_shipping, prefecture_id, shipping_period_id, shipping_method_id , price , item_status_id " do
+        expect(test_item).to be_valid
+      end
     end
 
-    it "invalid with a name that has more than 41 characters" do
-      test_item.name = Faker::Alphanumeric.alphanumeric(number: 41)
-      subject
-      expect(test_item.errors[:name]).to include("is too long (maximum is 40 characters)")
-    end
+    context 'cannot save' do
+      it "is invalid without a name" do
+        test_item.name = nil
+        subject
+        expect(test_item.errors[:name]).to include("can't be blank")
+      end
 
-    it "is invalid without a detail" do
-      test_item.detail = nil
-      subject
-      expect(test_item.errors[:detail]).to include("can't be blank")
-    end
+      it "invalid with a name that has more than 41 characters" do
+        test_item.name = Faker::Alphanumeric.alphanumeric(number: 41)
+        subject
+        expect(test_item.errors[:name]).to include("is too long (maximum is 40 characters)")
+      end
 
-    it "invalid with a detail that has more than 1001 characters" do
-      test_item.detail = Faker::Alphanumeric.alphanumeric(number: 1001)
-      subject
-      expect(test_item.errors[:detail]).to include("is too long (maximum is 1000 characters)")
-    end
+      it "is invalid without a detail" do
+        test_item.detail = nil
+        subject
+        expect(test_item.errors[:detail]).to include("can't be blank")
+      end
 
-    it "is invalid without a seller_id" do
-      test_item.seller_id = nil
-      subject
-      expect(test_item.errors[:seller_id]).to include("can't be blank")
-    end
+      it "invalid with a detail that has more than 1001 characters" do
+        test_item.detail = Faker::Alphanumeric.alphanumeric(number: 1001)
+        subject
+        expect(test_item.errors[:detail]).to include("is too long (maximum is 1000 characters)")
+      end
 
-    it "is invalid without a category_id" do
-      test_item.category_id = nil
-      subject
-      expect(test_item.errors[:category_id]).to include("can't be blank")
-    end
+      it "is invalid without a seller_id" do
+        test_item.seller_id = nil
+        subject
+        expect(test_item.errors[:seller_id]).to include("can't be blank")
+      end
 
-    it "is invalid without a condition_id" do
-      test_item.condition_id = nil
-      subject
-      expect(test_item.errors[:condition_id]).to include("can't be blank")
-    end
+      it "is invalid without a category_id" do
+        test_item.category_id = nil
+        subject
+        expect(test_item.errors[:category_id]).to include("can't be blank")
+      end
 
-    it "is invalid without true or false" do
-      test_item.is_seller_shipping = nil
-      subject
-      expect(test_item.errors[:is_seller_shipping]).to include("is not included in the list")
-    end
+      it "is invalid without a condition_id" do
+        test_item.condition_id = nil
+        subject
+        expect(test_item.errors[:condition_id]).to include("can't be blank")
+      end
 
-    it "is invalid without a prefecture_id" do
-      test_item.prefecture_id = nil
-      subject
-      expect(test_item.errors[:prefecture_id]).to include("can't be blank")
-    end
+      it "is invalid without true or false" do
+        test_item.is_seller_shipping = nil
+        subject
+        expect(test_item.errors[:is_seller_shipping]).to include("is not included in the list")
+      end
 
-    it "is invalid without a shipping_period_id" do
-      test_item.shipping_period_id = nil
-      subject
-      expect(test_item.errors[:shipping_period_id]).to include("can't be blank")
-    end
+      it "is invalid without a prefecture_id" do
+        test_item.prefecture_id = nil
+        subject
+        expect(test_item.errors[:prefecture_id]).to include("can't be blank")
+      end
 
-    it "is invalid without a shipping_method_id" do
-      test_item.shipping_method_id = nil
-      subject
-      expect(test_item.errors[:shipping_method_id]).to include("can't be blank")
-    end
+      it "is invalid without a shipping_period_id" do
+        test_item.shipping_period_id = nil
+        subject
+        expect(test_item.errors[:shipping_period_id]).to include("can't be blank")
+      end
 
-    it "is invalid without a price" do
-      test_item.price = nil
-      subject
-      expect(test_item.errors[:price]).to include("can't be blank")
-    end
+      it "is invalid without a shipping_method_id" do
+        test_item.shipping_method_id = nil
+        subject
+        expect(test_item.errors[:shipping_method_id]).to include("can't be blank")
+      end
 
-    it "is invalid a price without number" do
-      test_item.price = "aaaaaaaaaaaaaaaa"
-      subject
-      expect(test_item.errors[:price]).to include("is not included in the list")
-    end
+      it "is invalid without a price" do
+        test_item.price = nil
+        subject
+        expect(test_item.errors[:price]).to include("can't be blank")
+      end
 
-    it "is invalid a price more than 10000000" do
-      test_item.price = 10000000
-      subject
-      expect(test_item.errors[:price]).to include("is not included in the list")
-    end
+      it "is invalid a price without number" do
+        test_item.price = "aaaaaaaaaaaaaaaa"
+        subject
+        expect(test_item.errors[:price]).to include("is not included in the list")
+      end
 
-    it "is invalid a price less than 299" do
-      test_item.price = 299
-      subject
-      expect(test_item.errors[:price]).to include("is not included in the list")
-    end
+      it "is invalid a price more than 10000000" do
+        test_item.price = 10000000
+        subject
+        expect(test_item.errors[:price]).to include("is not included in the list")
+      end
+
+      it "is invalid a price less than 299" do
+        test_item.price = 299
+        subject
+        expect(test_item.errors[:price]).to include("is not included in the list")
+      end
 
 
-    it "is invalid without a item_status_id" do
-      test_item.item_status_id = nil
-      subject
-      expect(test_item.errors[:item_status_id]).to include("can't be blank")
-     end
+      it "is invalid without a item_status_id" do
+        test_item.item_status_id = nil
+        subject
+        expect(test_item.errors[:item_status_id]).to include("can't be blank")
+      end
     end
   end
 
@@ -178,7 +178,6 @@ describe Item do
       item.update(item_status_id: 2)
       expect(item.item_status_id).to eq 2
     end
-
   end
 
   describe '#index' do
